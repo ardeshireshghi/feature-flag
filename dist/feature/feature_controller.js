e47b8e3e72713f4b20f75b6c66950026d4bb50ba
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2020-01-22 02:25:13 +0000

    Add 'prettier' and 'ardi-cache'
    Use prettier for keeping the code syntax quality
    
    Add 'ardi-cache' npm module to abstract away caching of the features in `FeatureService`
    
    Prettier changes to syntax

--- dist/feature/feature_controller.js
+++ dist/feature/feature_controller.js
@@ -15,8 +15,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var feature = _config["default"].feature,
-    envName = _config["default"].envName;
+var feature = _config["default"].feature;
 var featureService = new _feature_service["default"]({
   bucketName: feature.s3Bucket
 });
--- dist/feature/feature_service.js
+++ dist/feature/feature_service.js
@@ -9,6 +9,8 @@ var _s = _interopRequireDefault(require("aws-sdk/clients/s3"));
 
 var _config = _interopRequireDefault(require("../config"));
 
+var _ardiCache = _interopRequireDefault(require("ardi-cache"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
@@ -31,20 +33,22 @@ var s3 = new _s["default"]();
 var feature = _config["default"].feature,
     envName = _config["default"].envName;
 var FEATURE_S3_KEY = "feature-service/".concat(envName, "-features.json");
+var CACHE_KEY = 'features';
 var CACHE_TTL_MINS = feature.cacheTTLMins;
-var featuresCache = {};
 
 var FeatureService =
 /*#__PURE__*/
 function () {
   function FeatureService(_ref) {
-    var bucketName = _ref.bucketName;
+    var bucketName = _ref.bucketName,
+        _ref$cacheProvider = _ref.cacheProvider,
+        cacheProvider = _ref$cacheProvider === void 0 ? (0, _ardiCache["default"])() : _ref$cacheProvider;
 
     _classCallCheck(this, FeatureService);
 
     this._bucketName = bucketName;
-
-    this._createS3KeyIfNotExists();
+    this._cache = cacheProvider.store('object');
+    this._featureS3KeyChecked = false;
   }
 
   _createClass(FeatureService, [{
@@ -56,6 +60,7 @@ function () {
         var _ref2,
             _ref2$useCache,
             useCache,
+            featuresCache,
             featuresDataRaw,
             featuresData,
             errorMessage,
@@ -67,41 +72,55 @@ function () {
               case 0:
                 _ref2 = _args.length > 0 && _args[0] !== undefined ? _args[0] : {}, _ref2$useCache = _ref2.useCache, useCache = _ref2$useCache === void 0 ? true : _ref2$useCache;
 
-                if (!(useCache && this._hasValidCache())) {
-                  _context.next = 3;
+                if (!this._featureS3KeyChecked) {
+                  _context.next = 4;
                   break;
                 }
 
-                return _context.abrupt("return", featuresCache.data);
+                _context.next = 4;
+                return this._createS3KeyIfNotExists();
 
-              case 3:
-                _context.prev = 3;
+              case 4:
                 _context.next = 6;
-                return s3.getObject(this._createParams()).promise();
+                return this._cache.get(CACHE_KEY);
 
               case 6:
-                featuresDataRaw = _context.sent.Body.toString('utf-8');
-                featuresData = JSON.parse(featuresDataRaw);
+                featuresCache = _context.sent;
 
-                if (useCache) {
-                  this._setCache(featuresData);
+                if (!(useCache && featuresCache !== null)) {
+                  _context.next = 9;
+                  break;
                 }
 
-                return _context.abrupt("return", featuresData);
+                return _context.abrupt("return", featuresCache);
+
+              case 9:
+                _context.prev = 9;
+                _context.next = 12;
+                return s3.getObject(this._createS3CallParams()).promise();
 
               case 12:
-                _context.prev = 12;
-                _context.t0 = _context["catch"](3);
+                featuresDataRaw = _context.sent.Body.toString('utf-8');
+                featuresData = JSON.parse(featuresDataRaw);
+                _context.next = 16;
+                return this._setCache(featuresData);
+
+              case 16:
+                return _context.abrupt("return", featuresData);
+
+              case 19:
+                _context.prev = 19;
+                _context.t0 = _context["catch"](9);
                 errorMessage = 'There was an error fetching features data from S3';
                 console.error(errorMessage, _context.t0);
                 throw new Error(errorMessage);
 
-              case 17:
+              case 24:
               case "end":
                 return _context.stop();
             }
           }
-        }, _callee, this, [[3, 12]]);
+        }, _callee, this, [[9, 19]]);
       }));
 
       function fetch() {
@@ -134,9 +153,10 @@ function () {
                 return this._persist(freshFeatureData);
 
               case 7:
-                this._setCache(freshFeatureData);
+                _context2.next = 9;
+                return this._setCache(freshFeatureData);
 
-              case 8:
+              case 9:
               case "end":
                 return _context2.stop();
             }
@@ -180,9 +200,10 @@ function () {
                 return this._persist(featureDataWithoutFeature);
 
               case 7:
-                this._setCache(featureDataWithoutFeature);
+                _context3.next = 9;
+                return this._setCache(featureDataWithoutFeature);
 
-              case 8:
+              case 9:
               case "end":
                 return _context3.stop();
             }
@@ -207,7 +228,7 @@ function () {
             switch (_context4.prev = _context4.next) {
               case 0:
                 _context4.next = 2;
-                return s3.putObject(_objectSpread({}, this._createParams(), {
+                return s3.putObject(_objectSpread({}, this._createS3CallParams(), {
                   Body: JSON.stringify(newFeatureBlob)
                 })).promise();
 
@@ -226,37 +247,89 @@ function () {
       return _persist;
     }()
   }, {
-    key: "_createParams",
-    value: function _createParams() {
+    key: "_createS3CallParams",
+    value: function _createS3CallParams() {
       return {
         Bucket: this._bucketName,
         Key: FEATURE_S3_KEY
       };
     }
   }, {
-    key: "_hasValidCache",
-    value: function _hasValidCache() {
-      return featuresCache.data && featuresCache.data.expiry >= Date.now();
-    }
-  }, {
     key: "_setCache",
-    value: function _setCache(featuresData) {
-      featuresCache = {
-        data: featuresData,
-        expiry: Date.now() + CACHE_TTL_MINS * 60 * 1000
-      };
-    }
+    value: function () {
+      var _setCache2 = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee5(featuresData) {
+        return regeneratorRuntime.wrap(function _callee5$(_context5) {
+          while (1) {
+            switch (_context5.prev = _context5.next) {
+              case 0:
+                _context5.next = 2;
+                return this._cache.put(CACHE_KEY, featuresData, CACHE_TTL_MINS);
+
+              case 2:
+              case "end":
+                return _context5.stop();
+            }
+          }
+        }, _callee5, this);
+      }));
+
+      function _setCache(_x4) {
+        return _setCache2.apply(this, arguments);
+      }
+
+      return _setCache;
+    }()
   }, {
     key: "_createS3KeyIfNotExists",
-    value: function _createS3KeyIfNotExists() {
-      var _this = this;
+    value: function () {
+      var _createS3KeyIfNotExists2 = _asyncToGenerator(
+      /*#__PURE__*/
+      regeneratorRuntime.mark(function _callee6() {
+        return regeneratorRuntime.wrap(function _callee6$(_context6) {
+          while (1) {
+            switch (_context6.prev = _context6.next) {
+              case 0:
+                _context6.prev = 0;
+                _context6.next = 3;
+                return s3.headObject(this._createS3CallParams()).promise();
 
-      s3.headObject(this._createParams(), function (err, metadata) {
-        if (err && err.code === 'NotFound') {
-          _this._persist({});
-        }
-      });
-    }
+              case 3:
+                _context6.next = 10;
+                break;
+
+              case 5:
+                _context6.prev = 5;
+                _context6.t0 = _context6["catch"](0);
+
+                if (!(_context6.t0 && _context6.t0.code === 'NotFound')) {
+                  _context6.next = 10;
+                  break;
+                }
+
+                _context6.next = 10;
+                return this._persist({});
+
+              case 10:
+                _context6.prev = 10;
+                this._featureS3KeyChecked = true;
+                return _context6.finish(10);
+
+              case 13:
+              case "end":
+                return _context6.stop();
+            }
+          }
+        }, _callee6, this, [[0, 5, 10, 13]]);
+      }));
+
+      function _createS3KeyIfNotExists() {
+        return _createS3KeyIfNotExists2.apply(this, arguments);
+      }
+
+      return _createS3KeyIfNotExists;
+    }()
   }]);
 
   return FeatureService;
