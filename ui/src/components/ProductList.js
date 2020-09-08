979a2ba3323f33b24dd2380f4f08f8a586fd2ef5
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2020-09-08 23:03:22 +0100

    Add confirm dialog to delete product

--- ui/src/components/ProductList.js
+++ ui/src/components/ProductList.js
@@ -12,6 +12,7 @@ import {
   minorScale,
   majorScale,
   Small,
+  Dialog,
   ApplicationsIcon,
   TrashIcon,
   useTheme,
@@ -23,13 +24,18 @@ function ProductItem({ product }) {
   const theme = useTheme();
   const [products, setProducts] = useProductState();
   const [isHovered, setHovered] = useState(false);
+  const [deleteProductDialogShown, setDeleteProductShown] = useState(false);
   let history = useHistory();
 
   const handleViewProductClick = () => {
     history.push(`/product/${product.name}/features`);
   };
 
-  const handleDeleteBtn = async () => {
+  const handleDeleteBtn = () => {
+    setDeleteProductShown(true);
+  };
+
+  const handleDeleteProduct = async () => {
     await deleteProduct({ name: product.name });
     setProducts(products.filter(({ name }) => name !== product.name));
 
@@ -37,107 +43,120 @@ function ProductItem({ product }) {
   };
 
   return (
-    <Pane
-      display="flex"
-      flexBasis="25%"
-      minWidth="260px"
-      paddingLeft={minorScale(2)}
-      paddingRight={minorScale(2)}
-      paddingBottom={majorScale(2)}
-    >
-      <Card
-        background="white"
-        display="flex"
-        flex="1"
-        flexDirection="column"
-        elevation={1}
-        hoverElevation={4}
-        cursor="pointer"
-        overflow="hidden"
-        transition="all 0.25s ease"
-        transform={isHovered ? 'translateY(-4px)' : 'none'}
-        style={{ willChange: 'box-shadow,opacity' }}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
+    <>
+      <Dialog
+        isShown={deleteProductDialogShown}
+        title="Delete product"
+        intent="danger"
+        onCloseComplete={() => setDeleteProductShown(false)}
+        onConfirm={handleDeleteProduct}
+        confirmLabel="Delete"
       >
-        <Pane borderBottom position="relative" minHeight="200px">
-          <ApplicationsIcon
-            position="relative"
-            left="50%"
-            transform="translateX(-50%) translateY(100px) scale(3)"
-            color="rgb(255 87 34 / 33%)"
-          />
-          <Pane
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            transition="opacity 0.25s ease"
-            style={{ willChange: 'opacity' }}
-            opacity={isHovered ? 1 : 0}
-            pointerEvents={isHovered ? 'auto' : 'none'}
-            backgroundColor={theme.scales.blue.B3}
-            position="absolute"
-            width="100%"
-            height="100%"
-            left="0"
-            top="0"
-          >
-            <Paragraph
-              marginLeft={majorScale(1)}
-              marginRight={majorScale(1)}
-              marginBottom={majorScale(4)}
-              textAlign="center"
-              transition="transform 0.25s ease, opacity 0.15s ease 0.1s"
-              transform={isHovered ? 'translateY(0)' : 'translateY(-20px)'}
-              style={{ willChange: 'transform,opacity' }}
+        Are you sure you want to delete "{product.name}"?
+      </Dialog>
+      <Pane
+        display="flex"
+        flexBasis="25%"
+        minWidth="260px"
+        paddingLeft={minorScale(2)}
+        paddingRight={minorScale(2)}
+        paddingBottom={majorScale(2)}
+      >
+        <Card
+          background="white"
+          display="flex"
+          flex="1"
+          flexDirection="column"
+          elevation={1}
+          hoverElevation={4}
+          cursor="pointer"
+          overflow="hidden"
+          transition="all 0.25s ease"
+          transform={isHovered ? 'translateY(-4px)' : 'none'}
+          style={{ willChange: 'box-shadow,opacity' }}
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+        >
+          <Pane borderBottom position="relative" minHeight="200px">
+            <ApplicationsIcon
+              position="relative"
+              left="50%"
+              transform="translateX(-50%) translateY(100px) scale(3)"
+              color="rgb(255 87 34 / 33%)"
+            />
+            <Pane
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+              transition="opacity 0.25s ease"
+              style={{ willChange: 'opacity' }}
+              opacity={isHovered ? 1 : 0}
+              pointerEvents={isHovered ? 'auto' : 'none'}
+              backgroundColor={theme.scales.blue.B3}
+              position="absolute"
+              width="100%"
+              height="100%"
+              left="0"
+              top="0"
             >
-              {product.description}
-            </Paragraph>
-            <Button
-              borderRadius="50px"
-              height={30}
-              appearance="primary"
-              onClick={handleViewProductClick}
-              transition="transform 0.25s ease, opacity 0.15s ease 0.1s"
-              transform={isHovered ? 'translateY(0)' : 'translateY(40px)'}
-              style={{ willChange: 'transform,opacity' }}
-            >
-              View Product
-            </Button>
-          </Pane>
-        </Pane>
-        <Pane alignItems="flex-end" display="flex" padding={majorScale(2)}>
-          <Pane>
-            <Paragraph size={500}>
-              {product.name}
-              <Small
-                opacity={isHovered ? '0' : '1'}
-                transition="transform 0.15s ease, opacity 0.1s ease 0.05s"
-                transform={isHovered ? 'translateY(20px)' : 'translateY(0)'}
+              <Paragraph
+                marginLeft={majorScale(1)}
+                marginRight={majorScale(1)}
+                marginBottom={majorScale(4)}
+                textAlign="center"
+                transition="transform 0.25s ease, opacity 0.15s ease 0.1s"
+                transform={isHovered ? 'translateY(0)' : 'translateY(-20px)'}
                 style={{ willChange: 'transform,opacity' }}
-                marginTop={minorScale(1)}
-                fontSize="0.625rem"
-                display="block"
-                color="#C7CED4"
-                textTransform="uppercase"
               >
-                Created {dateTimeAgo(product.createdAt)}
-              </Small>
-            </Paragraph>
+                {product.description}
+              </Paragraph>
+              <Button
+                borderRadius="50px"
+                height={30}
+                appearance="primary"
+                onClick={handleViewProductClick}
+                transition="transform 0.25s ease, opacity 0.15s ease 0.1s"
+                transform={isHovered ? 'translateY(0)' : 'translateY(40px)'}
+                style={{ willChange: 'transform,opacity' }}
+              >
+                View Product
+              </Button>
+            </Pane>
           </Pane>
-          <TrashIcon
-            title="Delete product"
-            transition="transform 0.25s ease 0.25s, opacity 0.15s ease 0.35s"
-            transform={isHovered ? 'translateY(0)' : 'translateY(40px)'}
-            color={theme.palette.neutral.base}
-            style={{ willChange: 'transform,opacity' }}
-            marginLeft="auto"
-            onClick={handleDeleteBtn}
-          />
-        </Pane>
-      </Card>
-    </Pane>
+          <Pane alignItems="flex-end" display="flex" padding={majorScale(2)}>
+            <Pane>
+              <Paragraph size={500}>
+                {product.name}
+                <Small
+                  opacity={isHovered ? '0' : '1'}
+                  transition="transform 0.15s ease, opacity 0.1s ease 0.05s"
+                  transform={isHovered ? 'translateY(20px)' : 'translateY(0)'}
+                  style={{ willChange: 'transform,opacity' }}
+                  marginTop={minorScale(1)}
+                  fontSize="0.625rem"
+                  display="block"
+                  color="#C7CED4"
+                  textTransform="uppercase"
+                >
+                  Created {dateTimeAgo(product.createdAt)}
+                </Small>
+              </Paragraph>
+            </Pane>
+            <TrashIcon
+              size={14}
+              title="Delete product"
+              transition="transform 0.15s ease-out 0.15s, opacity 0.1s ease-out 0.25s"
+              transform={isHovered ? 'translateY(0)' : 'translateY(40px)'}
+              color={theme.palette.neutral.base}
+              style={{ willChange: 'transform,opacity' }}
+              marginLeft="auto"
+              onClick={handleDeleteBtn}
+            />
+          </Pane>
+        </Card>
+      </Pane>
+    </>
   );
 }
 
@@ -154,7 +173,7 @@ function ProductList({ products }) {
     <Pane display="flex" flexDirection="column" flexGrow="1" alignItems="center" justifyContent="center">
       <ApplicationsIcon color={theme.scales.blue.B5} transform="scale(8)" />
       <Text color={theme.scales.neutral.N6A} position="relative" top={majorScale(9)}>
-            No products created yet
+        No products created yet
       </Text>
     </Pane>
   );
