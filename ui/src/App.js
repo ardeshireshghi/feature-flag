f91f03a56e42ffbe8992dee7811669526da45d94
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2020-09-08 13:16:54 +0100

    Introduce React Browser router
    Add routes for product list and the product features

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 import { Button } from 'evergreen-ui';
 import FeatureListPage from './pages/FeatureListPage';
+import ProductListPage from './pages/ProductListPage';
 import Nav from './components/Nav';
 
 import './App.css';
 
-function App() {
+function AppWithRouter() {
   return (
     <div className="App">
       <Nav />
-      <FeatureListPage />
+      <Router>
+        <Route exact path="/">
+          <ProductListPage />
+        </Route>
+        <Route exact path="/products">
+          <ProductListPage />
+        </Route>
+        <Route path="/product/:name">
+          <FeatureListPage />
+        </Route>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default AppWithRouter;
