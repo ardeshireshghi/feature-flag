ee5b56255b6a12bb6cdbe37230292bf5c6f69e7e
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2020-09-23 17:54:51 +0100

    Add authoriser middleware to the service
    Authoriser middleware function is used for protected endpoints to
    check the Auth Bearer JWT token provided by either web user or api client
    app and allow them to read the resources
    
    We use `x-client-type` header to decide to use user pool in Cognito or
    the client app pool

--- service/api/v1/lib/http/response.js
+++ service/api/v1/lib/http/response.js
@@ -11,5 +11,5 @@ export const responseError = (res, message, statusCode) => {
 export const addCORSHeaders = res => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-client-type');
 };
--- service/dist/api/v1/lib/http/response.js
+++ service/dist/api/v1/lib/http/response.js
@@ -18,7 +18,7 @@ exports.responseError = responseError;
 const addCORSHeaders = res => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-client-type');
 };
 
 exports.addCORSHeaders = addCORSHeaders;
