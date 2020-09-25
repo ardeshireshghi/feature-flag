2d64324e256585ceb8fb2cf4c04061be3d559db6
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2020-09-25 11:42:34 +0100

    Improve build UI script

--- scripts/build-ui.sh
+++ scripts/build-ui.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -eox pipefail
 
 source ./scripts/shared.sh
 
+set +e
 feature_flag_api_endpoint="$(service_endpoint)"
-
 [[ -z "$feature_flag_api_endpoint" ]] && echo "cannot find feature service endpoint from serverless info, make sure the service is deployed" && exit 1
+set -e
 
 export REACT_APP_FEATURE_FLAG_SERVICE_BASE_URL="$feature_flag_api_endpoint"
 export REACT_APP_COGNITO_CLIENT_ID="$cognito_client_id"
